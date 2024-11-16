055d2f4325b136168726d801686cbba835b4202e
Leander Merbecks <leander.merbecks@tum.de>
2024-11-16 14:44:38 +0000

    Added cost run function refactured main function

--- src/pv_power_matching.py
+++ src/pv_power_matching.py
@@ -280,7 +280,7 @@ def map_populations_to_orientation(real_population: np.ndarray, integer_populati
     return panel_orientations
 
 
-def calculate_electricity_cost(power_consumed: np.ndarray, time_step_hours: float, electricity_cost:float=35e-2, electricity_retail_price=8e-2) -> float:
+def calculate_electricity_cost(power_consumed: np.ndarray, time_step_hours: float, electricity_cost: float=35e-2, electricity_retail_price:float=8e-2) -> float:
     """calculates the electricity cost of a day operating the power
     system and home together. 
 
@@ -289,14 +289,13 @@ def calculate_electricity_cost(power_consumed: np.ndarray, time_step_hours: floa
         (negative entries mean power has been sold to the grid)
         time_step_hours (float): constant time step for the power array
         electricity_cost (float, optional): Cost of buying electricity
-        from the grid in €/kWh. Defaults to 35e-2.
-        electricity_retail_price (_type_, optional): Price for selling
-        electricity to the grid in €/kWh. Defaults to 8e-2.
+        from the grid in €/kWh. Defaults to 35e-2. 
+        electricity_retail_price (float, optional): Retail price for
+        selling electricity from the grid in €/kWh. Defaults to 8e-2.
 
     Returns:
-        float: Total cost of electricity
+        float: Total cost of electricity. 
     """
-    
 
     energy_consumed = power_consumed * time_step_hours * 1e-3 # kWh
     buying_energy_mask = energy_consumed > 0
@@ -311,7 +310,7 @@ def calculate_electricity_cost(power_consumed: np.ndarray, time_step_hours: floa
     return cost
 
 
-def objective_function(real_population, integer_population, permutation_population):
+def objective_function(real_population, integer_population, permutation_population, **kwargs):
     panel_orientations = map_populations_to_orientation(
         real_population, integer_population)
     power_supply_characteristic = pv_system_power_production_characteristic(
@@ -319,19 +318,21 @@ def objective_function(real_population, integer_population, permutation_populati
     power_consumed = power_demand_characteristic - power_supply_characteristic
     time_step = (demand_times[1] - demand_times[0]) * 24
     cost = calculate_electricity_cost(
-        power_consumed, time_step_hours=time_step)
+        power_consumed, time_step_hours=time_step, **kwargs)
     sse = sum_of_squared_errors(
         power_demand_characteristic, power_supply_characteristic)
     return cost
 
 
-def plot_result_characteristics(real_population, integer_population):
+def plot_result_characteristics(real_population, integer_population, ax_power:plt.axes=None):
     panel_orientations = map_populations_to_orientation(
         real_population, integer_population)
 
     power_supply_characteristic = pv_system_power_production_characteristic(
         panel_orientation=panel_orientations)
-    _, ax_power = plt.subplots(nrows=1, ncols=1)
+    if isinstance(ax_power, type(None)):
+        fig_chars, ax_power = plt.subplots(nrows=1, ncols=1)
+        
     ax_power.plot(demand_times*24, power_demand_characteristic, label='Demand curve')
     ax_power.plot(demand_times*24, power_supply_characteristic,
              label='Production curve')
@@ -375,12 +376,39 @@ def ga_results(Rbest, Ibest, Pbest, PI_best, PI_best_progress):
     plt.show()
     return
 
+def different_costs_run(cost_limits:tuple, price_limits:tuple, num_runs:int=9):
+    axis_dimension = int(np.sqrt(num_runs))
+    num_runs = axis_dimension*axis_dimension
+    cost_axis = np.linspace(cost_limits[0], cost_limits[1], axis_dimension)
+    price_axis = np.linspace(price_limits[0], price_limits[1], axis_dimension)
+    costs, prices = np.meshgrid(cost_axis, price_axis)
+    costs_flat = costs.flatten()
+    prices_flat = prices.flatten()
+    total_costs = []
+    angles = []
+    used_panels = []
+    histories = []
+    
+    for index in range(num_runs):
+        PI_best, Rbest, Ibest, _, PI_best_progress = optimize_pv_system(num_gen=200, num_pop=1000, electricity_cost=costs_flat[index], electricity_price=prices_flat[index])
+        total_costs.append(PI_best)
+        angles.append(Rbest)
+        used_panels.append(Ibest)
+        histories.append(PI_best_progress)
+    
+    figure_costs, axes_costs = plt.subplots(nrows=axis_dimension, ncols=axis_dimension)
+    for axis, realpop, ipop in zip(axes_costs, angles, used_panels):
+        plot_result_characteristics(realpop, ipop, ax_power=axis)
+    plt.show()
+    
+    pass
 
-def main():
-    # ga_results(np.deg2rad(np.array([60, 60, 300, 60, 60, 300, 60, 60, 300, 60, 60, 300, 60, 60, 300, 60, 60, 300, 80, 40, 60, 80, 40, 60, 80, 40, 60, 80, 40, 60, 80, 40, 60, 80, 40, 60])),np.array([18]), 0,0,0)
-    number_of_generations = 200
-    number_of_populations = 1000
-    max_number_of_panels = 20
+
+
+def optimize_pv_system(num_gen:int, num_pop:int, max_num_panels:int=40, electricity_cost:float=35e-2, electricity_price:float=8e-2):
+    number_of_generations = num_gen
+    number_of_populations = num_pop
+    max_number_of_panels = max_num_panels
     number_of_real_variables = max_number_of_panels * 2
     number_of_integer_variables = 1
     number_of_permutation_variables = 0
@@ -404,9 +432,15 @@ def main():
         [tournament_probability, crossover_probability, mutation_probability])
 
     PI_best, Rbest, Ibest, Pbest, PI_best_progress = ga_min(
-        FILENAME, OBJECTIVE_FUNCTION, size_parameters, integer_variable_limits, real_variable_limits, probability_parameters)
-    ga_results(Rbest, Ibest, Pbest, PI_best, PI_best_progress)
+        objective_function, size_parameters, integer_variable_limits, real_variable_limits, probability_parameters, electricity_cost=electricity_cost, electricity_retail_price=electricity_price)
+        
+    return PI_best,Rbest,Ibest,Pbest,PI_best_progress
 
+def main():
+    # ga_results(np.deg2rad(np.array([60, 60, 300, 60, 60, 300, 60, 60, 300, 60, 60, 300, 60, 60, 300, 60, 60, 300, 80, 40, 60, 80, 40, 60, 80, 40, 60, 80, 40, 60, 80, 40, 60, 80, 40, 60])),np.array([18]), 0,0,0)
+    # PI_best, Rbest, Ibest, Pbest, PI_best_progress = optimize_pv_system(num_gen=200, num_pop=1000)
+    # ga_results(Rbest, Ibest, Pbest, PI_best, PI_best_progress)
+    different_costs_run(cost_limits=[20e-2, 50e-2], price_limits=[-84e-2, 1])
 
 if __name__ == '__main__':
     main()
